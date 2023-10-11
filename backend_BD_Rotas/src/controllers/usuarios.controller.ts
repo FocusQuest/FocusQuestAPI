e43f3505327980c7f4414bf928115405dfdc899b
Gustavo <tron20118@gmail.com>
2023-10-11 18:52:11 -0300

    login no lugar certo/token/rotas emplementadas

--- backend_BD_Rotas/src/controllers/usuarios.controller.ts
+++ backend_BD_Rotas/src/controllers/usuarios.controller.ts
@@ -1,22 +1,32 @@
-import { Request, Response, NextFunction } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-export const getUsuarios = async (req: Request, res: Response, next: NextFunction) => {
+export const getUsuarios = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const usuarios = await prisma.usuario.findMany({
-        // a linha abaixo inclui a tabela chamado para o usuário
+      // a linha abaixo inclui a tabela chamado para o usuário
       // include: { chamados: true }
     });
     res.json({ usuarios });
-// res.json({ usuarios, chamados })
+    // res.json({ usuarios, chamados })
   } catch (error) {
     next(error);
   }
 };
 
-export const getUsuarioById = async (req: Request, res: Response, next: NextFunction) => {
+export const getUsuarioById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const { id } = req.params;
     const usuario = await prisma.usuario.findUnique({
@@ -25,7 +35,7 @@ export const getUsuarioById = async (req: Request, res: Response, next: NextFunc
       },
     });
     if (!usuario) {
-      return res.status(404).json({ message: 'Usuário não encontrado!' });
+      return res.status(404).json({ message: "Usuário não encontrado!" });
     }
     res.json(usuario);
   } catch (error) {
@@ -33,7 +43,11 @@ export const getUsuarioById = async (req: Request, res: Response, next: NextFunc
   }
 };
 
-export const createUsuario = async (req: Request, res: Response, next: NextFunction) => {
+export const createUsuario = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const usuario = await prisma.usuario.create({
       data: {
@@ -49,7 +63,11 @@ export const createUsuario = async (req: Request, res: Response, next: NextFunct
   }
 };
 
-export const deleteUsuario = async (req: Request, res: Response, next: NextFunction) => {
+export const deleteUsuario = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const { id } = req.params;
     const deletedUser = await prisma.usuario.delete({
@@ -63,14 +81,18 @@ export const deleteUsuario = async (req: Request, res: Response, next: NextFunct
   }
 };
 
-export const updateUsuario = async (req: Request, res: Response, next: NextFunction) => {
+export const updateUsuario = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const { id } = req.params;
     const updatedUsuario = await prisma.usuario.update({
       where: {
         id: Number(id),
       },
-      data: req.body
+      data: req.body,
     });
     res.json(updatedUsuario);
   } catch (error) {
@@ -78,3 +100,118 @@ export const updateUsuario = async (req: Request, res: Response, next: NextFunct
   }
 };
 
+//Codigo encryptado
+/*export const loginUsuario = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { emailUsuario, senhaUsuario } = req.params;
+    const usuario = await prisma.usuario.findUnique({
+      where: {
+        emailUsuario,
+      },
+    });
+    if (!usuario) {
+      return res.status(401).json({ message: "Usuário nao encontrado" });
+    }
+    const senhaEncryptada = await bcrypt.hash(senhaUsuario, 10);
+
+    const senha = await bcrypt.compare(senhaUsuario, usuario.senhaUsuario);
+    if (!senha) {
+      return res.status(401).json({ message: "Usuário ou senha incorretos" });
+    }
+    const token = jwt.sign({ id: usuario.id }, "secret", { expiresIn: "1h" });
+  } catch (error) {
+    next(error);
+  }
+};*/
+export const loginUsuario = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (req.params.nivelAcesso === "3") {
+    try {
+      const usuario = await prisma.usuario.findUnique({
+        where: {
+          emailUsuario: req.body.emailUsuario,
+        },
+      });
+      if (!usuario) {
+        res.status(404).json({ msg: "Usuário não encontrado" });
+      }
+      const senha = req.body.senhaUsuario;
+      if (senha !== usuario?.senhaUsuario) {
+        res.status(401).json({ msg: "Senha incorreta" });
+      }
+      const tokenUsuario = jwt.sign(
+        { id: usuario?.id },
+        process.env.JWT_PASS ?? "",
+        { expiresIn: "24h" },
+      );
+      res.status(202).json({
+        msg: "Usuário logado com sucesso",
+        usuario: usuario,
+        token: tokenUsuario,
+      });
+    } catch (error) {
+      next(error);
+    }
+  } else if (req.params.nivelAcesso === "2") {
+    try {
+      const suporte = await prisma.suporte.findUnique({
+        where: {
+          emailSuporte: req.body.emailSuporte,
+        },
+      });
+      if (!suporte) {
+        res.status(404).json({ msg: "Usuário não encontrado" });
+      }
+      const senha = req.body.senhaUsuario;
+      if (senha !== suporte?.senhaSuporte) {
+        res.status(401).json({ msg: "Senha incorreta" });
+      }
+      const tokenSuporte = jwt.sign(
+        { id: suporte?.id },
+        process.env.JWT_PASS ?? "",
+        { expiresIn: "24h" },
+      );
+      res.status(202).json({
+        msg: "Suporte logado com sucesso",
+        usuario: suporte,
+        token: tokenSuporte,
+      });
+    } catch (error) {
+      next(error);
+    }
+  } else if (req.params.nivelAcesso === "1") {
+    try {
+      const admin = await prisma.admin.findUnique({
+        where: {
+          emailAdmin: req.body.emailAdmin,
+        },
+      });
+      if (!admin) {
+        res.status(404).json({ msg: "Usuário não encontrado" });
+      }
+      const senha = req.body.senhaAdmin;
+      if (senha !== admin?.senhaAdmin) {
+        res.status(401).json({ msg: "Senha incorreta" });
+      }
+      const tokenAdmin = jwt.sign(
+        { id: admin?.id },
+        process.env.JWT_PASS ?? "",
+        { expiresIn: "24h" },
+      );
+      res.status(202).json({
+        msg: "Usuário logado com sucesso",
+        usuario: admin,
+        token: tokenAdmin,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+};
--- backend_BD_Rotas/src/routes/api.router.ts
+++ backend_BD_Rotas/src/routes/api.router.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -9,93 +9,31 @@ import {
   createChamado,
   deleteChamado,
   updateChamado,
-} from '../controllers/chamados.controller';
+} from "../controllers/chamados.controller";
 import {
   getUsuarios,
   getUsuarioById,
   createUsuario,
   deleteUsuario,
   updateUsuario,
-} from '../controllers/usuarios.controller';
+  loginUsuario,
+} from "../controllers/usuarios.controller";
 
 const router = express.Router();
 
 // Chamados routes
-router.get('/chamados', getChamados);
-router.get('/chamados/:id', getChamadoById);
-router.post('/chamados', createChamado);
-router.delete('/chamados/:id', deleteChamado);
-router.patch('/chamados/:id', updateChamado);
+router.get("/chamados", getChamados);
+router.get("/chamados/:id", getChamadoById);
+router.post("/chamados", createChamado);
+router.delete("/chamados/:id", deleteChamado);
+router.patch("/chamados/:id", updateChamado);
 
 // Usuarios routes
-router.get('/usuarios', getUsuarios);
-router.get('/usuarios/:id', getUsuarioById);
-router.post('/usuarios', createUsuario);
-router.delete('/usuarios/:id', deleteUsuario);
-router.patch('/usuarios/:id', updateUsuario);
-
-
-
-//Autenticador de Login usuario, admin e suporte
-router.post(
-  "/usuarios/login/:nivelAcesso",
-  async (req: Request, res: Response, next: NextFunction) => {
-    if (req.params.nivelAcesso === "3") {
-      try {
-        const usuario = await prisma.usuario.findUnique({
-          where: {
-            emailUsuario: req.body.emailUsuario,
-          },
-        });
-        if (!usuario) {
-          res.status(404).json({ msg: "Usuário não encontrado" });
-        }
-        const senha = req.body.senhaUsuario;
-        if (senha !== usuario?.senhaUsuario) {
-          res.status(401).json({ msg: "Senha incorreta" });
-        }
-        res.status(202).json(usuario);
-      } catch (error) {
-        next(error);
-      }
-    } else if (req.params.nivelAcesso === "2") {
-      try {
-        const suporte = await prisma.suporte.findUnique({
-          where: {
-            emailSuporte: req.body.emailSuporte,
-          },
-        });
-        if (!suporte) {
-          res.status(404).json({ msg: "Usuário não encontrado" });
-        }
-        const senha = req.body.senhaUsuario;
-        if (senha !== suporte?.senhaSuporte) {
-          res.status(401).json({ msg: "Senha incorreta" });
-        }
-        res.status(202).json(suporte);
-      } catch (error) {
-        next(error);
-      }
-    } else if (req.params.nivelAcesso === "1") {
-      try {
-        const admin = await prisma.admin.findUnique({
-          where: {
-            emailAdmin: req.body.emailAdmin,
-          },
-        });
-        if (!admin) {
-          res.status(404).json({ msg: "Usuário não encontrado" });
-        }
-        const senha = req.body.senhaAdmin;
-        if (senha !== admin?.senhaAdmin) {
-          res.status(401).json({ msg: "Senha incorreta" });
-        }
-        res.status(202).json(admin);
-      } catch (error) {
-        next(error);
-      }
-    }
-  },
-);
+router.get("/usuarios", getUsuarios);
+router.get("/usuarios/:id", getUsuarioById);
+router.post("/usuarios", createUsuario);
+router.delete("/usuarios/:id", deleteUsuario);
+router.patch("/usuarios/:id", updateUsuario);
+router.post("/usuarios/login/:nivelAcesso", loginUsuario);
 
 export default router;
