af846e3072d786d7f34c100deef86a8c97089c2e
Marcus Betti <marcusbetti@gmail.com>
2023-10-12 18:51:34 -0300

    Definicao do arquivo Meus_chamados.tsx para obter todos os chamados de um usuário específico. ainda é preciso fazer a logica de pegar o id do usuário a partir do login do usuario. o valor do idUsuario esta sendo passado como o valor de uma variavel por enquanto. Foi testado

--- src/usuario/components/Meus_chamados.tsx
+++ src/usuario/components/Meus_chamados.tsx
@@ -1,10 +1,46 @@
-function Chamados(){
-    return (
-        <div>
-            <h4> Esta é a página Meus chamados</h4>
-        </div>
-    )
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Chamado {
+  id: number;
+  nomeChamado: string;
+  descChamado: string;
+  dataAberturaChamado: string;
+  // Include other properties here
+}
+
+function Chamados() {
+  const [chamados, setChamados] = useState<Chamado[]>([]);
+  const userId = 2; // Replace with the actual user ID value
+
+  useEffect(() => {
+    const fetchChamados = async () => {
+      try {
+        const response = await axios.get<Chamado[]>(`http://localhost:3000/chamados/usuario/${userId}`);
+        setChamados(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchChamados();
+  }, [userId]);
+
+  return (
+    <div>
+      <h4>Esta é a página Meus chamados</h4>
+      <ul>
+        {chamados.map(chamado => (
+          <li key={chamado.id}>
+            <h5>{chamado.nomeChamado}</h5>
+            <p>{chamado.descChamado}</p>
+            <p>{chamado.dataAberturaChamado}</p>
+            {/* Render other properties here */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Chamados;
-
