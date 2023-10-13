c858564af1e07e1858bbf22fcb7ffc93b9788fad
Marcus Betti <marcusbetti@gmail.com>
2023-10-13 17:45:04 -0300

    Atualizacao de Formulario.tsx com criacao de chamados

--- src/usuario/components/pages/Formulario.tsx
+++ src/usuario/components/pages/Formulario.tsx
@@ -1,34 +1,17 @@
 import React from "react";
 import { useForm } from 'react-hook-form';
 import '../css/estilos.css';
-// import { createChamado } from "../../../../../Backend_atual/FocusQuestAPI/backend_BD_Rotas/src/controllers/chamados.controller"
+
 import axios from 'axios';
 
 type FormData = {
-  idUsuario: 2, // incluir abaixo
-  idCategoria: string;
+  idUsuario: number,
+  idCategoria: number;
   nomeChamado: string;
   descChamado: string;
-  idLab: string;
-  idComputador: string;
+  idLab: number;
+  idComputador: number;
 };
-
-// JSON criado pela req:
-// {
-//   "idCategoria":"sem internet",
-//   "idUsuario":"2",
-//   "nomeChamado":"teste assunto",
-//   "descChamado":"teste descricao",
-//   "idLab":"teste local",
-//   "idComputador":"teste computador"
-// }
-
-//OK nomeChamado: req.body.nomeChamado,
-//OK descChamado: req.body.descChamado,
-//OK idUsuario: req.body.idUsuario,
-//OK idLab: req.body.idLab,
-//OK idComputador: req.body.idComputador,
-// idCategoria: req.body.idCategoria,
 
 const FormTeste: React.FC = () => {
   const {
@@ -37,36 +20,51 @@ const FormTeste: React.FC = () => {
     formState: { errors },
   } = useForm<FormData>();
 
-    const onSubmit = async (data: FormData) => {
-    alert(JSON.stringify(data));
+  const onSubmit = async (data: FormData) => {
+    try {
+      const idUsuario = parseInt('2', 10); // Define idUsuario as a variable
+      const idCategoria = data.idCategoria;
+      const idLab = data.idLab;
+      const idComputador = data.idComputador; // Convert idComputador to a number
+      data.idUsuario = idUsuario; // Assign the value of idUsuario to the data object
+      data.idCategoria = idCategoria; // Assign the converted idCategoria to the data object
+      data.idLab = idLab; // Assign the converted idLab to the data object
+      data.idComputador = idComputador; // Assign the converted idComputador to the data object
+      console.log('Dados do formulário:', data); // Log the form data
+      const response = await axios.post('http://localhost:3000/chamados', data);
+      console.log('Resposta:', response); // Log the response
+      if (response.status === 201) {
+        const createdChamado = response.data;
+        // Do something with the created chamado object
+        console.log('Chamado:', createdChamado); // For example, log the created chamado
+      } else {
+        // Handle the error case
+        console.log('Erro ao criar chamado');
+      }
+    } catch (error) {
+      // Handle any network or other errors
+      console.error('Error:', error);
+    }
   };
+
 
   return (
     <div className="Container">
       <div className="Label">
-        <label>Busca serviço</label>
+        <label>Tipo de serviço</label>
         <select
           className={errors?.idCategoria && "input-error"}
           defaultValue="0"
-          {...register("idCategoria", { validate: (value) => value !== "0" })}
+          {...register("idCategoria", { validate: (value) => value !== 0 })}
         >
           <option value="0">Selecione...</option>
-          <option value="sem internet">Sem internet</option>
-          <option value="software">Software</option>
-          <option value="outros">Outros</option>
+          <option value="1">Sem internet</option>
+          <option value="2">Software</option>
+          <option value="3">Outros</option>
         </select>
       </div>
       
       {/* idUsuario deve ser puxado dos dados de login */}
-      {/* <div className="Label">
-        <label>Identificador do Usuário</label>
-        <input
-          type="hidden"
-          value="2"
-          placeholder="idUsuario"
-          {...register("idUsuario")}
-        />
-      </div> */}
 
       <div className="Label">
         <label>Assunto</label>
@@ -87,6 +85,7 @@ const FormTeste: React.FC = () => {
         <label>Descrição</label>
         <textarea
           className={errors?.descChamado && "input-error"}
+          // type="text"
           placeholder="Descrição do problema"
           {...register("descChamado", {
             required: true,
@@ -112,7 +111,7 @@ const FormTeste: React.FC = () => {
         <label>Identificador do computador</label>
         <input
           type="text"
-          placeholder="IP"
+          placeholder="ID do computador"
           {...register("idComputador")}
         />
       </div>
@@ -134,26 +133,5 @@ const FormTeste: React.FC = () => {
   );
 };
 
-
-
-
-const onSubmit = async (data: FormData) => {
-  try {
-    console.log('Form data:', data); // Log the form data
-    const response = await axios.post('http://localhost:3000/chamados', data);
-    console.log('Response:', response); // Log the response
-    if (response.status === 201) {
-      const createdChamado = response.data;
-      // Do something with the created chamado object
-      console.log('Created chamado:', createdChamado); // For example, log the created chamado
-    } else {
-      // Handle the error case
-      console.log('Error creating chamado');
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error('Error:', error);
-  }
-};
 
 export default FormTeste;
