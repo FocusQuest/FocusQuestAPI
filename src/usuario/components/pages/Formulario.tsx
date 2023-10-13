4f065cc44699af6d6cb72e8efce89722ee7fd412
mvbetti <73323386+marcusvbe@users.noreply.github.com>
2023-10-13 12:49:46 -0300

    Update Formulario.tsx

--- src/usuario/components/pages/Formulario.tsx
+++ src/usuario/components/pages/Formulario.tsx
@@ -1,88 +1,157 @@
 import React from "react";
 import { useForm } from 'react-hook-form';
-import '../css/estilos.css';
+import '../styles.css'
+import { createChamado } from "../../../../../Backend_atual/FocusQuestAPI/backend_BD_Rotas/src/controllers/chamados.controller"
+import axios from 'axios';
 
 type FormData = {
-  problema: string;
-  assunto: string;
-  descricao: string;
-  local: string;
-  identificador: string;
+  idUsuario: 2, // incluir abaixo
+  idCategoria: string;
+  nomeChamado: string;
+  descChamado: string;
+  idLab: string;
+  idComputador: string;
 };
+
+// JSON criado pela req:
+// {
+//   "idCategoria":"sem internet",
+//   "idUsuario":"2",
+//   "nomeChamado":"teste assunto",
+//   "descChamado":"teste descricao",
+//   "idLab":"teste local",
+//   "idComputador":"teste computador"
+// }
+
+//OK nomeChamado: req.body.nomeChamado,
+//OK descChamado: req.body.descChamado,
+//OK idUsuario: req.body.idUsuario,
+//OK idLab: req.body.idLab,
+//OK idComputador: req.body.idComputador,
+// idCategoria: req.body.idCategoria,
 
 const FormTeste: React.FC = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<FormData>();
 
-    const onSubmit = (data: FormData) => {
+    const onSubmit = async (data: FormData) => {
     alert(JSON.stringify(data));
   };
 
   return (
-      <><div className="Label">
-      <label>Busca serviço</label>
-      <select
-        className={errors?.problema && "input-error"}
-        defaultValue="0"
-        {...register("problema", { validate: (value) => value !== "0" })}
-      >
-        <option value="0">Selecione...</option>
-        <option value="developer">Sem internet</option>
-        <option value="other">Software</option>
-        <option value="other">Outros</option>
-      </select>
-    </div><div className="Label">
+    <div className="Container">
+      <div className="Label">
+        <label>Busca serviço</label>
+        <select
+          className={errors?.idCategoria && "input-error"}
+          defaultValue="0"
+          {...register("idCategoria", { validate: (value) => value !== "0" })}
+        >
+          <option value="0">Selecione...</option>
+          <option value="sem internet">Sem internet</option>
+          <option value="software">Software</option>
+          <option value="outros">Outros</option>
+        </select>
+      </div>
+
+{/* idUsuario deve ser puxado dos dados de login */}
+      <div className="Label">
+  <label>Identificador do Usuário</label>
+  <input
+    type="hidden"
+    value="2"
+    {...register("idUsuario")}
+  />
+</div>
+
+      <div className="Label">
         <label>Assunto</label>
         <input
-          className={errors?.assunto && "input-error"}
+          className={errors?.nomeChamado && "input-error"}
           type="text"
-          placeholder="Assunto"
-          {...register("assunto", {
+          placeholder="nomeChamado"
+          {...register("nomeChamado", {
             required: true,
-          })} />
-        {errors?.assunto?.type === "required" && (
+          })}
+        />
+        {errors?.nomeChamado?.type === "required" && (
           <p className="error-message">Campo obrigatório</p>
         )}
-      </div><div className="Label">
+      </div>
+
+      <div className="Label">
         <label>Descrição</label>
         <input
-          className={errors?.descricao && "input-error"}
+          className={errors?.descChamado && "input-error"}
           type="text"
-          placeholder="Descrição"
-          {...register("descricao", {
+          placeholder="descChamado"
+          {...register("descChamado", {
             required: true,
-          })} />
-        {errors?.descricao?.type === "required" && (
+          })}
+        />
+        {errors?.descChamado?.type === "required" && (
           <p className="error-message">Campo obrigatório</p>
         )}
-      </div><div className="Label">
+      </div>
+
+      <div className="Label">
         <label>Local</label>
         <input
           type="text"
-          placeholder="Local"
-          {...register("local")} />
-      </div><div className="Label">
-        <label>Identificador da máquina</label>
+          placeholder="idLab"
+          {...register("idLab")}
+        />
+      </div>
+
+      <div className="Label">
+        <label>Identificador do computador</label>
         <input
           type="text"
           placeholder="IP"
-          {...register("identificador")} />
-      </div><div className="Label">
+          {...register("idComputador")}
+        />
+      </div>
+
+      <div className="Label">
         <div className="anexo">
           <label>Anexo</label>
           <input
             type="file"
-            placeholder="Clique aqui para selecionar os arquivos ou arraste-os aqui" />
+            placeholder="Clique aqui para selecionar os arquivos ou arraste-os aqui"
+          />
         </div>
-      </div><div className="Label">
+      </div>
+
+      <div className="Label">
         <button onClick={handleSubmit(onSubmit)}>Enviar</button>
-      </div></>
-    
+      </div>
+    </div>
   );
+};
+
+
+
+
+const onSubmit = async (data: FormData) => {
+  try {
+    console.log('Form data:', data); // Log the form data
+    const response = await axios.post('http://localhost:3000/chamados', data);
+    console.log('Response:', response); // Log the response
+    if (response.status === 201) {
+      const createdChamado = response.data;
+      // Do something with the created chamado object
+      console.log('Created chamado:', createdChamado); // For example, log the created chamado
+    } else {
+      // Handle the error case
+      console.log('Error creating chamado');
+    }
+  } catch (error) {
+    // Handle any network or other errors
+    console.error('Error:', error);
+  }
 };
 
 export default FormTeste;
