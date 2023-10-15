cb008e8161b8e1d330d208f99cc2d35f2f736634
Silmara <silmarabittencourt13@gmail.com>
2023-10-14 22:15:23 -0300

    Estilização das páginas da visão do administrador

--- src/adm/pages/admin.tsx
+++ src/adm/pages/admin.tsx
@@ -1,9 +1,38 @@
-function Admin(){
-    return (
-        <div>
-            <h4> Esta é a página Administração</h4>
-        </div>
-    )
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/estilos.css';
+
+function Admin() {
+  return (
+    <div className="containerAdmin">
+      <h2>Administração</h2>
+      <hr></hr>
+      <div className="button-container">
+        <Link to="/criar-usuario" className="admin-link">
+          <button className="admin-button" style={{ backgroundColor: '#5E5E5E' }}>
+            Criar Usuário
+          </button>
+        </Link>
+        <Link to="/redefinir-senha" className="admin-link">
+          <button className="admin-button" style={{ backgroundColor: '#7E7E7E' }}>
+            Redefinir Senha
+          </button>
+        </Link>
+        <Link to="/adicionar-ferramenta" className="admin-link">
+          <button className="admin-button" style={{ backgroundColor: '#8E8E8E' }}>
+            Adicionar Ferramenta
+          </button>
+        </Link>
+        <Link to="/adicionar-servico" className="admin-link">
+          <button className="admin-button" style={{ backgroundColor: '#A0A0A0' }}>
+            Adicionar Serviço
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
+
+
--- src/adm/pages/ferramentas.tsx
+++ src/adm/pages/ferramentas.tsx
@@ -1,9 +1,37 @@
-function Ferramentas(){
-    return (
-        <div>
-            <h4> Esta é a página ferramentas</h4>
-        </div>
-    )
+import React from 'react';
+
+function Ferramentas() {
+  return (
+    <div className="containerFAQ">
+      <h2>Ferramentas</h2>
+      <hr></hr>
+      <div className="faq-item">
+        <h3>Software 1</h3>
+        <p>
+          Este é um software educacional muito útil. Você pode encontrá-lo no site oficial da empresa. Siga os passos abaixo para baixá-lo:
+        </p>
+        <ol>
+          <li>Acesse o site oficial do Software 1 em: <a href="https://www.software1.com" target="_blank" rel="noopener noreferrer">https://www.software1.com</a></li>
+          <li>Navegue até a seção de downloads.</li>
+          <li>Clique no botão "Baixar agora" e siga as instruções para instalar o software em seu computador.</li>
+        </ol>
+      </div>
+
+      <div className="faq-item">
+        <h3>Software 2</h3>
+        <p>
+          O Software 2 é uma ferramenta útil para tarefas escolares. Para baixá-lo, siga estas etapas:
+        </p>
+        <ol>
+          <li>Visite o site oficial do Software 2 em: <a href="https://www.software2.com" target="_blank" rel="noopener noreferrer">https://www.software2.com</a></li>
+          <li>Procure a seção de download.</li>
+          <li>Clique no link de download e siga as instruções de instalação.</li>
+        </ol>
+      </div>
+
+      {/* Adicione mais informações sobre outros softwares escolares conforme necessário */}
+    </div>
+  );
 }
 
 export default Ferramentas;
